6b1df58907b532be7a473df24e20265eb005869c
phuongias <phng2912@gmail.com>
2024-07-02 21:23:18 +0200

    Papier Hand animiert

--- schnickschnackschnuck.js
+++ schnickschnackschnuck.js
@@ -2,16 +2,62 @@
 
 const canvas = document.querySelector(".webgl");
 const scene= new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 
+let mixer; //für die Animation
+let mixer2;
+
+//HandWaveRight
 const loader = new THREE.GLTFLoader();
-loader.load('assets/handFinished.glb', (glb) => {
+loader.load('assets/paperHandAnimated.glb', (glb) => {
     console.log(glb)
-    /*const root = gltf.scene;
-    scene.add(root);*/
+    //const root = gltf.scene;
+    //scene.add(root);
+    const handWave = glb.scene;
 
-    const root = glb.scene;
-    root.scale.set(0.1, 0.1, 0.1);
-    scene.add(root);
+
+    handWave.scale.set(0.1, 0.1, 0.1);
+    handWave.rotation.set(0,0,20)
+    handWave.position.set(5,0,-2)
+
+    scene.add(handWave);
+
+    mixer = new THREE.AnimationMixer(handWave);
+
+    const clips = glb.animations;
+    const clip = THREE.AnimationClip.findByName(clips, 'HandWave'); //auswählen welche Animation abgespielt werden soll
+    const action = mixer.clipAction(clip);
+    action.play();
+
+}, function (xhr){
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+},function (error){
+    console.log(error);
+    console.log('An error happened');
+})
+
+
+//HandWaveLeft
+const loader2 = new THREE.GLTFLoader();
+loader2.load('assets/paperHandAnimated.glb', (glb) => {
+    console.log(glb)
+    //const root = gltf.scene;
+    //scene.add(root);
+    const handWaveLeft = glb.scene;
+
+
+    handWaveLeft.scale.set(0.1, 0.1, 0.1);
+    handWaveLeft.rotation.set(0,0,-20);
+    handWaveLeft.position.set(-5,0,-2)
+
+    scene.add(handWaveLeft);
+
+    mixer2 = new THREE.AnimationMixer(handWaveLeft);
+
+    const clips2 = glb.animations;
+    const clip2 = THREE.AnimationClip.findByName(clips2, 'HandWave'); //auswählen welche Animation abgespielt werden soll
+    const actionLeft = mixer2.clipAction(clip2);
+    actionLeft.play();
 
 }, function (xhr){
     console.log((xhr.loaded / xhr.total * 100) + '% loaded')
@@ -25,6 +71,7 @@ light.position.set(2,2,5)
 scene.add(light);
 
 
+//Box als Test
 /*const geometry = new THREE.BoxGeometry(1,1,1);
 const material = new THREE.MeshBasicMaterial({color: 'red'});
 const boxMesh = new THREE.Mesh(geometry, material);
@@ -50,9 +97,12 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.shadowMap.enabled = true;
 renderer.gammaOutput = true;
 
-
+const clock = new THREE.Clock();
 function animate(){
     requestAnimationFrame(animate);
+    const delta = clock.getDelta()
+    mixer.update(delta);
+    mixer2.update(delta);
     renderer.render(scene,camera);
 }
 
